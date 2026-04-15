c48708e56724c3e40cfc930b5c02b1d5607eeb16
dreamdream318 <dreambusiness58@gmail.com>
2026-04-15 15:45:11 +0500

    Add Main component to render NoraeStore

---
+++ Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NoraeStore from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NoraeStore />
+  </React.StrictMode>
+);
